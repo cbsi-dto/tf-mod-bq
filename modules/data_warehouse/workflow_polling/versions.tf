f7db94a97b4072f54d636bf7bf0ddc2ba89511d0
abhishek010796 <abhishek.kumar@cbsinteractive.com>
2026-06-08 17:53:23 +0530

    Update provider version constraints to support v6.x for resource_tags
    - Update all sub-module version constraints from '< 6' to '< 7'
    - Enables use of provider v6.x which has resource_tags support for BigQuery
    - Required for enforced tagging feature (maintained_by=terraform)

--- modules/data_warehouse/workflow_polling/versions.tf
+++ modules/data_warehouse/workflow_polling/versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.52, < 6"
+      version = ">= 4.59, < 7"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
